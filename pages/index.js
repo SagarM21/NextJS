fc2d7305b697e84359b677f72ca56153c6ef6a8b
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2021-09-23 14:25:31 +0530

    Completed

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
-import Head from "next/head";
+import { server } from "../config";
+import ArticleList from "../components/ArticleList";
+import Meta from "../components/Meta";
 
-export default function Home() {
+export default function Home({ articles }) {
+  //console.log(articles);
   return (
     <div>
-      <Head>
-        <title>WebDev</title>
-        <meta
-          name="Web Development"
-          content="web development, programming"
-        ></meta>
-      </Head>
-      <h1>Welcome to Next JS</h1>
+      <Meta />
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`);
+  const articles = await res.json();
+  return {
+    props: {
+      articles,
+    },
+  };
+};
